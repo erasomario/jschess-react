4d86abf2a1edc5a8731f39be5cb6a32b5ab8d84b
Mario Eraso <erasomario@qualisys.com.co>
2021-07-20 22:49:28 -0500

    bug with all spaces string

--- src/components/users/UsersList.js
+++ src/components/users/UsersList.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { useAuth } from '../../providers/ProvideAuth'
 import ListGroup from 'react-bootstrap/ListGroup'
 import { useInput } from '../../hooks/useInput';
@@ -9,58 +9,69 @@ import 'simplebar/dist/simplebar.min.css';
 
 export default function UserList({ onSelect = (a) => a, style, focus }) {
 
+    const textRef = useRef()
+    const [text, setText] = useState("")
     const { user } = useAuth()
     const [list, setList] = useState([])
     const [selected, setSelected] = useState(null)
 
-    const [props, , grabFocus] = useInput()
     const [error, setError] = useState();
 
-    useEffect(() => {        
-            grabFocus()        
-    }, [grabFocus])
+    useEffect(() => {
+        textRef.current && textRef.current.focus()
+    }, [])
 
     useEffect(() => {
-        if (props.value.length >= 3) {
-            findUsersLike(props.value, user.api_key)
+        if (text.length >= 3) {
+            findUsersLike(text, user.api_key)
                 .then(data => { setList(data.filter((u) => u.id !== user.id)); setError() })
                 .catch(e => setError(e.message))
         } else {
             setError()
             setList([])
         }
-    }, [user, props.value])
+    }, [user, text])
 
     return <>
         <Form.Group>
-            <Form.Control {...props} placeholder="Escriba parte del nombre" />
+            <Form.Control
+                onKeyDown={e => e.key === " " ? e.preventDefault() : null}
+                onChange={e => setText(e.target.value.replace(/\s/g, ""))}
+                value={text}
+                ref={textRef}
+                placeholder="Escriba parte del nombre" />
         </Form.Group>
 
-        <SimpleBar style={style}>
-            {list.length > 0 && <ListGroup>
-                {list && list.map((u) => <ListGroup.Item
-                    key={u.id}
-                    active={selected && selected.id === u.id}
-                    onClick={() => { setSelected(u); onSelect(u) }}
-                    style={{ cursor: 'pointer' }}
-                >{u.username}</ListGroup.Item>)}
-            </ListGroup >
-            }
-            {(list.length === 0 && props.value.length < 3) && <div style={{ height: '100%', display: 'table-cell', verticalAlign: 'middle' }}>
+        {list.length > 0 &&
+            <SimpleBar style={style}>
+                <ListGroup>
+                    {list && list.map((u) => <ListGroup.Item
+                        key={u.id}
+                        active={selected && selected.id === u.id}
+                        onClick={() => { setSelected(u); onSelect(u) }}
+                        style={{ cursor: 'pointer' }}
+                    >{u.username}</ListGroup.Item>)}
+                </ListGroup >
+            </SimpleBar>
+        }
+        {(list.length === 0 && text.length < 3) &&
+
+            <div style={{ ...style, display: "flex", justifyContent: "flex-start", alignItems: "center" }}>
                 <div>
                     Escriba al menos 3 letras para empezar a buscar
                 </div>
             </div>}
-            {(list.length === 0 && props.value.length >= 3) && <div style={{ height: '100%', display: 'table-cell', verticalAlign: 'middle' }}>
+        {(!error && list.length === 0 && text.length >= 3) &&
+            <div style={{ ...style, display: "flex", justifyContent: "flex-start", alignItems: "center" }}>
                 <div>
-                    No se encontraron jugadores similares a "{props.value}"
+                    No se encontraron jugadores similares a "{text}"
                 </div>
             </div>}
-            {error && <div style={{ height: '100%', display: 'table-cell', verticalAlign: 'middle' }}>
-                <div>
-                    {error}
-                </div>
-            </div>}
-        </SimpleBar>
+        {error && <div style={{ height: '100%', display: 'table-cell', verticalAlign: 'middle' }}>
+            <div>
+                {error}
+            </div>
+        </div>}
+
     </>
 }
