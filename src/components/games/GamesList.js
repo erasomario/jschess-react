a3f80ce9f92ffc772828117f13becff429a2a47b
Mario Eraso <erasomario@qualisys.com.co>
2021-08-05 23:07:24 -0500

    General GameList was made a component on its own

---
+++ src/components/games/GamesList.js
@@ -0,0 +1,76 @@
+import React, { useLayoutEffect, useRef } from "react"
+import { ListGroup } from "react-bootstrap"
+import { FaMedal, FaPlus } from "react-icons/fa"
+import SimpleBar from 'simplebar-react'
+import 'simplebar/dist/simplebar.min.css'
+
+const Pawn = ({ color }) => {
+    return <div className="pawn" style={{ backgroundImage: `url('${process.env.PUBLIC_URL}/assets/${color}p.svg')` }} />
+}
+
+const Loading = ({ style }) => {
+    return <div style={style}>Cargando</div>
+}
+
+const NoData = ({ type, style }) => {
+    return <div style={{ ...style, display: "flex", flexDirection: "column", justifyContent: "center", gap: "1em" }}>
+        <b>{type === "open" ? "No tiene partidas en curso" : "Aun no tiene partidas finalizadas"}</b>
+        <div>Puede iniciar un juego contra amigos o contra el computador en la opción <FaPlus /> del inicio.</div>
+        <div style={{ height: "5em" }}></div>
+    </div>
+}
+
+const GameItem = React.forwardRef((props, ref) => {
+
+    const { game: g, onSelect } = props
+
+
+    return <ListGroup.Item
+        ref={g.selected ? ref : null}
+        active={g.selected}
+        onClick={() => onSelect(g.id)}
+        style={{ cursor: 'pointer', margin: "0", padding: "0.4em" }}>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <div>
+                <div className="playerSubRow">
+                    <Pawn color={'w'} />{g.whiteName}
+                    {(g.whiteHighlight && !g.ended) && <div className="gameListDot" />}
+                    {(g.whiteHighlight && g.ended) && <FaMedal style={{ marginLeft: "0.5em" }} />}
+                </div>
+                <div className="playerSubRow">
+                    <Pawn color={'b'} />{g.blackName}
+                    {(g.blackHighlight && !g.ended) && <div className="gameListDot" />}
+                    {(g.blackHighlight && g.ended) && <FaMedal style={{ marginLeft: "0.5em" }} />}
+                </div>
+            </div>
+            {g.isNew && <div className="gameListNew">Nueva</div>}
+            {g.ended &&
+                <div style={{ display: "flex", flexDirection: "column", alignItems: "flex-end", justifyContent: "center" }}>
+                    {g.draw && <div>Empate</div>}
+                    {new Date(g.createdAt).toLocaleDateString("es-CO")}
+                </div>
+            }
+        </div>
+    </ListGroup.Item>
+})
+
+export function GamesList({ data, height, onSelect, loading }) {
+    const ref = useRef()
+    useLayoutEffect(() => {
+        ref.current?.scrollIntoView({ block: "nearest", behavior: "auto" })
+    })
+    return <>
+        {loading && <Loading style={{ height }} />}
+        {!loading && data?.length === 0 && <NoData style={{ height }} type="open" />}
+        {!loading && data?.length > 0 &&
+            <SimpleBar style={{ height }}>
+                <ListGroup>
+                    {data.map(g => <GameItem
+                        key={g.id} game={g}
+                        onSelect={onSelect}
+                        ref={ref} />)}
+                </ListGroup>
+            </SimpleBar>
+        }
+    </>
+}
--- src/components/games/PlayerGamesList.js
+++ src/components/games/PlayerGamesList.js
@@ -1,78 +1,25 @@
-import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
-import { Alert } from 'react-bootstrap';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Modal from 'react-bootstrap/Modal';
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
-import { FaMedal, FaPlus } from 'react-icons/fa';
-import SimpleBar from 'simplebar-react';
-import 'simplebar/dist/simplebar.min.css';
-import { findGameById, setOpponentNotification } from '../../clients/game-client';
-import { findGamesByStatus } from '../../clients/user-client';
-import { useAuth } from '../../providers/ProvideAuth';
-import { useGame } from '../../providers/ProvideGame';
-import { getAsGameList } from './GamesListLogic';
-import "./PlayerGamesList.css";
+import React, { useEffect, useState } from 'react'
+import { Alert } from 'react-bootstrap'
+import Modal from 'react-bootstrap/Modal'
+import Tab from 'react-bootstrap/Tab'
+import Tabs from 'react-bootstrap/Tabs'
 
-const Loading = ({ style }) => {
-    return <div style={style}>Cargando</div>
-}
-
-const NoData = ({ type, style }) => {
-    return <div style={{ ...style, display: "flex", flexDirection: "column", justifyContent: "center", gap: "1em" }}>
-        <b>{type === "open" ? "No tiene partidas en curso" : "Aun no tiene partidas finalizadas"}</b>
-        <div>Puede iniciar un juego contra amigos o contra el computador en la opción <FaPlus /> del inicio.</div>
-        <div style={{ height: "5em" }}></div>
-    </div>
-}
-
-const Pawn = ({ color }) => {
-    return <div className="pawn" style={{ backgroundImage: `url('${process.env.PUBLIC_URL}/assets/${color}p.svg')` }} />
-}
-
-const GameItem = React.forwardRef((props, ref) => {
-
-    const { game: g, onSelect } = props
-
-    return <ListGroup.Item
-        ref={g.selected ? ref : null}
-        active={g.selected}
-        onClick={() => onSelect(g.id)}
-        style={{ cursor: 'pointer', margin: "0", padding: "0.4em" }}>
-        <div style={{ display: "flex", justifyContent: "space-between" }}>
-            <div>
-                <div className="playerSubRow">
-                    <Pawn color={'w'} />{g.whiteName}
-                    {(g.whiteHighlight && !g.ended) && <div className="gameListDot" />}
-                    {(g.whiteHighlight && g.ended) && <FaMedal style={{ marginLeft: "0.5em" }} />}
-                </div>
-                <div className="playerSubRow">
-                    <Pawn color={'b'} />{g.blackName}
-                    {(g.blackHighlight && !g.ended) && <div className="gameListDot" />}
-                    {(g.blackHighlight && g.ended) && <FaMedal style={{ marginLeft: "0.5em" }} />}
-                </div>
-            </div>
-            {g.isNew && <div className="gameListNew">Nueva</div>}
-            {g.ended &&
-                <div style={{ display: "flex", flexDirection: "column", alignItems: "flex-end", justifyContent: "center" }}>
-                    {g.draw && <div>Empate</div>}
-                    {new Date(g.createdAt).toLocaleDateString("es-CO")}
-                </div>
-            }
-        </div>
-    </ListGroup.Item>
-})
+import { findGameById, setOpponentNotification } from '../../clients/game-client'
+import { findGamesByStatus } from '../../clients/user-client'
+import { useAuth } from '../../providers/ProvideAuth'
+import { useGame } from '../../providers/ProvideGame'
+import { GamesList } from './GamesList'
+import { getAsGameList } from './GamesListLogic'
+import "./PlayerGamesList.css"
 
 export default function PlayerGamesList({ show, onHide = a => a }) {
-    const ref = useRef()
     const { user } = useAuth()
     const { game, updateGame } = useGame()
     const [error, setError] = useState(null)
     const [openGames, setOpenGames] = useState(null)
     const [closedGames, setClosedGames] = useState(null)
     const [loading, setLoading] = useState(true)
-
-    const scroll = useCallback(() => { ref.current?.scrollIntoView({ block: "nearest", behavior: "auto" }) }, [])
+    const [tab, setTab] = useState()
 
     useEffect(() => {
         if (show) {
@@ -82,7 +29,6 @@ export default function PlayerGamesList({ show, onHide = a => a }) {
                 .then(() => setError())
                 .then(() => setLoading(false))
                 .catch(e => setError(e.message))
-                .finally(scroll)
 
             findGamesByStatus(user.id, user.api_key, "closed")
                 .then(l => getAsGameList(l, game, user))
@@ -90,9 +36,17 @@ export default function PlayerGamesList({ show, onHide = a => a }) {
                 .then(() => setError())
                 .then(() => setLoading(false))
                 .catch(e => setError(e.message))
-                .finally(scroll)
+
         }
-    }, [show, game, user, scroll])
+    }, [show, game, user])
+
+    useEffect(() => {
+        if (openGames && (openGames.filter(e => e.id === game.id).length > 0)) {
+            setTab("open")
+        } else if (closedGames && (closedGames.filter(e => e.id === game.id).length > 0)) {
+            setTab("closed")
+        }
+    }, [game?.id, openGames, closedGames])
 
     const select = async gameId => {
         try {
@@ -116,34 +70,12 @@ export default function PlayerGamesList({ show, onHide = a => a }) {
             </div>
         </Modal.Header>
         <Modal.Body>
-            <Tabs defaultActiveKey="open" className="mb-3">
-                <Tab eventKey="open" title="En curso">
-                    {loading && <Loading style={{ height }} />}
-                    {!loading && openGames?.length === 0 && <NoData style={{ height }} type="open" />}
-                    {!loading && openGames?.length > 0 &&
-                        <SimpleBar style={{ height }}>
-                            <ListGroup>
-                                {openGames.map(g => <GameItem
-                                    key={g.id} game={g}
-                                    onSelect={select}
-                                    ref={ref} />)}
-                            </ListGroup>
-                        </SimpleBar>
-                    }
+            <Tabs activeKey={tab} onSelect={t => { setTab(t); setError() }} className="mb-3">
+                <Tab eventKey="open" title="En curso" >
+                    <GamesList data={openGames} loading={loading} height={height} onSelect={select} />
                 </Tab>
                 <Tab eventKey="closed" title="Finalizadas">
-                    {loading && <Loading style={{ height }} />}
-                    {!loading && closedGames?.length === 0 && <NoData style={{ height }} type="closed" />}
-                    {!loading && closedGames?.length > 0 &&
-                        <SimpleBar style={{ height }}>
-                            <ListGroup>
-                                {closedGames.map(g => <GameItem
-                                    key={g.id} game={g}
-                                    onSelect={select}
-                                    ref={ref} />)}
-                            </ListGroup>
-                        </SimpleBar>
-                    }
+                    <GamesList data={closedGames} loading={loading} height={height} onSelect={select} />
                 </Tab>
             </Tabs>
             {error && <Alert variant="danger">{error}</Alert>}
