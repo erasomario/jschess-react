fbb755dcf71602fe6b7f8056316426c03aa02289
Mario Eraso <erasomario@qualisys.com.co>
2021-08-09 11:14:52 -0500

    to show a list of the games being player now

---
+++ src/components/games/CurrentGamesList.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react'
+import { Alert } from 'react-bootstrap'
+import Modal from 'react-bootstrap/Modal'
+import { findCurrentGames, findGameById } from '../../clients/game-client'
+import { useAuth } from '../../providers/ProvideAuth'
+import { useGame } from '../../providers/ProvideGame'
+import { GamesList } from './GamesList'
+import { getAsGameList } from './GamesListLogic'
+
+export default function CurrentGamesList({ show, onHide = a => a }) {
+    const { user } = useAuth()
+    const { game, updateGame } = useGame()
+    const [error, setError] = useState(null)
+    const [games, setGames] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        if (show) {
+        findCurrentGames(user.api_key)
+                .then(l => getAsGameList(l, game, user))
+                .then(setGames)
+                .then(() => setError())
+                .then(() => setLoading(false))
+                .catch(e => setError(e.message))
+        }
+    }, [show, game, user])
+   
+    const select = async gameId => {
+        try {
+            const game = await findGameById(gameId, user.api_key)
+            updateGame(game)
+            onHide()
+        } catch (e) {
+            setError(e.message)
+        }
+    }
+
+    const height = "20em"
+    return <Modal show={show} onHide={() => onHide()}>
+        <Modal.Header closeButton>
+            <div style={{ overflow: 'hidden' }} >
+                <h4 style={{ display: "inline" }} className='align-top'>Partidas</h4>
+            </div>
+        </Modal.Header>
+        <Modal.Body>
+            <GamesList data={games} loading={loading} height={height} onSelect={select} />
+            {error && <Alert variant="danger">{error}</Alert>}
+        </Modal.Body>
+    </Modal>
+}
---
+++ src/components/games/OpenCurrentGamesButton.js
@@ -0,0 +1,17 @@
+import { useState } from "react"
+import { Button } from "react-bootstrap"
+import { FaGlasses } from "react-icons/fa"
+import CurrentGamesList from "./CurrentGamesList"
+
+
+function OpenCurrentGamesButton() {
+    const [showDialog, setShowDialog] = useState(false)
+    return <>
+        <CurrentGamesList show={showDialog} onHide={() => { setShowDialog(false) }}></CurrentGamesList>
+        <Button style={{ position: "relative" }} variant="primary" onClick={() => setShowDialog(true)}>
+            <FaGlasses style={{ marginTop: -4 }} ></FaGlasses>
+        </Button>
+    </>
+}
+
+export default OpenCurrentGamesButton
