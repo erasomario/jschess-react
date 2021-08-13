8c4ad2c505ad593078a10924431e913e1e1e547b
Mario Eraso <erasomario@qualisys.com.co>
2021-08-13 09:47:34 -0500

    added dynamic width

--- src/utils/Drawer.js
+++ src/utils/Drawer.js
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react"
 import "./Drawer.css"
 
-const Drawer = ({ children, show = false, onHide = a => a, width, position = "right" }) => {
+const Drawer = ({ children, show = false, onHide = a => a, width, position = "right", animationSecs = 0.25 }) => {
 
     const bgOverlayRef = useRef()
     const drawerRef = useRef()
@@ -16,35 +16,37 @@ const Drawer = ({ children, show = false, onHide = a => a, width, position = "ri
             if (show) {
                 bgOverlayRef.current.style.visibility = "visible"
                 bgOverlayRef.current.style.opacity = "1"
-                bgOverlayRef.current.style.animation = "fadeIn 0.2s ease-out"
+                bgOverlayRef.current.style.animation = `fadeIn ${animationSecs}s ease-out`
 
-                drawerRef.current.style.visibility = "visible"
                 drawerRef.current.style.width = width
-                drawerRef.current.style.animation = `slideIn 0.2s ease-out`
+                drawerRef.current.style.visibility = "visible"
             } else {
                 bgOverlayRef.current.style.visibility = "hidden"
                 bgOverlayRef.current.style.opacity = "0"
-                bgOverlayRef.current.style.animation = "fadeOut 0.2s linear"
+                bgOverlayRef.current.style.animation = `fadeOut ${animationSecs}s ease-out`
 
-                drawerRef.current.style.visibility = "hidden"
                 drawerRef.current.style.width = "0"
-                drawerRef.current.style.animation = `slideOut 0.2s linear`
+                setTimeout(() => {
+                    drawerRef.current.style.visibility = "hidden"
+                }, animationSecs * 1000)
             }
             shown.current = show
         }
-    }, [position, show, width])
+    }, [position, show, width, animationSecs])
 
     const drawerStyle = {
         top: "0",
-        position: "absolute", backgroundColor: "white", width, height: "100%",
+        position: "absolute", backgroundColor: "white", width: "0", height: "100%",
         boxShadow: "0px 0px 2px 2px rgba(0, 0, 0, 0.2)",
         visibility: "hidden",
-        overflowX: "hidden"
+        overflowX: "hidden",
+        transition: `width ${animationSecs}s ease-out`
     }
-    drawerStyle[position] = `0`
+    drawerStyle[position] = "0"
     return <>
         <div ref={bgOverlayRef} onClick={onHide}
             style={{ visibility: "hidden", top: "0", left: "0", position: "absolute", backgroundColor: "rgba(0, 0, 0, 0.4)", width: "100%", height: "100%" }} ></div>
+
         <div ref={drawerRef}
             style={drawerStyle} >
             <div style={(position === "left" ? { width, position: "absolute", right: "0" } : { width, position: "absolute", left: "0" })}>
