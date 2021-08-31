dec98ea973c9924ae4cc59fc765c1f172a223664
Mario Eraso <erasomario@qualisys.com.co>
2021-08-30 22:07:08 -0500

    added support for text plain responses

--- src/utils/ApiClient.js
+++ src/utils/ApiClient.js
@@ -4,29 +4,35 @@ const getAddress = () => {
 }
 
 function apiRequest(endPoint, method, key, body) {
-    let opts = { method };
+    let opts = {method}
     if (body) {
         if (body instanceof File) {
             const formData = new FormData()
             formData.append('attachment', body)
-            opts = { body: formData, ...opts }
+            opts = {body: formData, ...opts}
         } else {
             opts = { headers: { 'Content-Type': "application/json" }, body: JSON.stringify(body), ...opts }
         }
     }
 
     if (key) {
-        opts.headers = { 'Authorization': `Bearer ${key}`, ...opts.headers }
+        opts.headers = {'Authorization': `Bearer ${key}`, ...opts.headers}
     }
 
     return fetch(getAddress() + "/api" + endPoint, opts)
         .then(response => {
-            var contentType = response.headers.get("content-type");
+            const contentType = response.headers.get("content-type")
             if (contentType && contentType.indexOf("application/json") !== -1) {
                 if (response.ok) {
                     return response.json()
                 } else {
                     return response.json().then(e => Promise.reject(Error(e?.error || `Error Inesperado ${response.status}`)))
+                }
+            } else if (contentType && contentType.indexOf("text/html") !== -1) {
+                if (response.ok) {
+                    return response.text()
+                } else {
+                    return Promise.reject(Error(`Error Inesperado ${response.status}`))
                 }
             } else {
                 if (response.ok) {
@@ -38,4 +44,4 @@ function apiRequest(endPoint, method, key, body) {
         })
 }
 
-export { apiRequest, getAddress }
+export {apiRequest, getAddress}
